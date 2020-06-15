bfbf8c4f628a20e0e12966a5b1d4d9aaef4d5ac6
rohithpalagiri <rohithpalagiri@gmail.com>
2020-06-15 12:12:21 -0400

    country project

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const Finder = ({handleSearchChange, value}) => {
+  return(
+    <div>Find Countries <input onChange={handleSearchChange} value={value} /></div>
+  )
+}
+
+const Result = ({filteredCountries}) => {
+  let result;
+  // console.log("these are the filtered countries", filteredCountries);
+  if (filteredCountries.length > 10){
+    result = <p>Too many mtches, specify another filter</p>
+  } else if (filteredCountries.length <=10 && filteredCountries.length >1){
+    result = <div>{filteredCountries.map((x) => x.name)}</div>
+  } else if(filteredCountries.length === 1){
+    result = (
+      <div>
+        <h1>{filteredCountries[0].name}</h1>
+        <div>{filteredCountries.capital}</div>
+      </div>
+    )
+  }
+
+  return(
+    <div>
+      {result}
     </div>
-  );
+  )
+}
+
+const App = () => {
+  const [ countries, setCountries ] = useState([])
+  const [ search, setSearch ] = useState('')
+  const [ results, setResults ] = useState([])
+
+  useEffect(() => {
+    axios
+      .get('https://restcountries.eu/rest/v2/all')
+      .then(response => {
+        setCountries(response.data)
+      })
+  }, [])
+
+  const handleSearchChange = (event) =>{
+    setSearch(event.target.value)
+    console.log('target value is: ', event.target.value);
+    
+    console.log("search value", search)
+    searchCountries();
+  }
+
+  const searchCountries = () =>{
+    const filteredCountries = countries.filter((x) => x.name.toLowerCase().includes(search.toLowerCase()));
+    setResults(filteredCountries);
+  }
+
+  return(
+    <div>
+    Find Countries <input onChange={handleSearchChange} value={search} />
+    <Result filteredCountries={results} />
+    </div>
+  )
 }
 
 export default App;
